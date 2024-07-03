7b8e6ac5c8d564e8d7dc69ff7f7dbf73af1983a9
miko859 <miko.doro@gmail.com>
2024-07-03 18:59:22 +0200

    Small fix

--- Assets/InventorySlot.cs
+++ Assets/InventorySlot.cs
@@ -6,10 +6,11 @@ using UnityEngine.EventSystems;
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData){
-
+        if (transform.childCount == 0) {
         GameObject dropped = eventData.pointerDrag;
         Drag drag = dropped.GetComponent<Drag>();
         drag.parentAfterDrag = transform;
+        }
     }
 
 }

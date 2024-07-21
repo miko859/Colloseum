701e17fa501ba39345f3c4b6e3bd000ae7712cee
Tomáš <tomas.tejiscak@gmail.com>
2024-07-21 11:59:58 +0200

    created animation and prefab for BossDoor

--- Assets/Scripts/SceneScrips/DoorController/DoorScript.cs
+++ Assets/Scripts/SceneScrips/DoorController/DoorScript.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class DoubleDoorController : MonoBehaviour
 {
+    public Animator animator;
+
     public Transform doorLeft;      // The left door object
     public Transform doorRight;     // The right door object
 
-    public float leftOpenZPosition;  // The z position where the left door should move when opened
-    public float rightOpenZPosition; // The z position where the right door should move when opened
+    public Vector3 leftOpenPosition;  // The position where the left door should move when opened
+    public Vector3 rightOpenPosition; // The position where the right door should move when opened
     private Vector3 leftClosedPosition;  // The initial position of the left door
     private Vector3 rightClosedPosition; // The initial position of the right door
 
-    public float speed = 2.0f;       // Speed at which the doors move
+    
+
+    public float speed = 1;       // Speed at which the doors move
     private bool isOpen = false;     // Condition to check if the doors are open
 
     void Start()
@@ -26,15 +30,9 @@ public class DoubleDoorController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.O))
         {
             // Toggle the door state
-            isOpen = !isOpen;
+            //isOpen = !isOpen;
+            animator.SetBool("openDoor", true);
+            
         }
-
-        // Determine the target z positions
-        float leftTargetZ = isOpen ? leftOpenZPosition : leftClosedPosition.z;
-        float rightTargetZ = isOpen ? rightOpenZPosition : rightClosedPosition.z;
-
-        // Move the doors to their target positions, keeping x and y constant
-        doorLeft.position = new Vector3(doorLeft.position.x, doorLeft.position.y, Mathf.Lerp(doorLeft.position.z, leftTargetZ, Time.deltaTime * speed));
-        doorRight.position = new Vector3(doorRight.position.x, doorRight.position.y, Mathf.Lerp(doorRight.position.z, rightTargetZ, Time.deltaTime * speed));
     }
 }

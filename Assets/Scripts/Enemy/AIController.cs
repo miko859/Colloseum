53e9d1f40c09db503ad40a3f13ef0deb1d16815b
Tomáš <tomas.tejiscak@gmail.com>
2024-07-23 20:42:24 +0200

    added Draw to show line on path in navmesh and calc for distance of entity from object

--- Assets/Scripts/Enemy/AIController.cs
+++ Assets/Scripts/Enemy/AIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,13 +8,17 @@ public class AIController : MonoBehaviour
 {
     public GameObject player;
     private NavMeshAgent agent;
+    private NavMeshPath path;
+    private Vector3[] pathCorners;
+    private float fullDistance = 0f;
     bool x = false;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        agent = GetComponent<NavMeshAgent>(); // Corrected line
+        agent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();       
 
         if (agent == null)
         {
@@ -31,7 +36,15 @@ public class AIController : MonoBehaviour
     {
         if (agent != null && player != null)
         {
-            x = agent.SetDestination(player.transform.position);
+            CalculateDistanceOfPath();
+
+            agent.CalculatePath(player.transform.position, path);
+            if (path.status == NavMeshPathStatus.PathComplete && fullDistance <= 20)
+            {
+                x = agent.SetDestination(player.transform.position);
+            }
+            Debug.Log(fullDistance);
+            /*
             Debug.Log(x);
             Debug.Log("Setting destination to player position: " + player.transform.position);
             Debug.Log("Current agent position: " + agent.transform.position);
@@ -39,7 +52,39 @@ public class AIController : MonoBehaviour
 
             // Additional checks
             Debug.Log("Agent path status: " + agent.pathStatus);
-            Debug.Log("Agent remaining distance: " + agent.remainingDistance);
+            Debug.Log("Agent remaining distance: " + agent.remainingDistance);*/
+            OnDrawGizmos();
+            
+        }
+    }
+
+
+    //shows path in Scene
+    private void OnDrawGizmos()
+    {
+        if (agent.destination != null)
+        {
+            Gizmos.color = Color.red;
+            {
+                // Draw lines joining each path corner
+                pathCorners = agent.path.corners;
+
+                for (int i = 0; i < pathCorners.Length - 1; i++)
+                {
+                    Gizmos.DrawLine(pathCorners[i], pathCorners[i + 1]);
+                }
+
+            }
+        }
+    }
+
+    private void CalculateDistanceOfPath()
+    {
+        fullDistance = 0f;
+        int i;
+        for (i = 1; i < pathCorners.Length; i++)
+        {
+            fullDistance += Vector3.Distance(pathCorners[i - 1], pathCorners[i]);
         }
     }
 }

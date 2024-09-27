c2b49a10d999d57a71e1833810d51e0c4d19f7ca
Tomáš Tejiščák <tomas.tejiscak@kosickaakademia.sk>
2024-09-27 06:49:50 +0200

    made public mouseSensitivity easier to modify and later can be used to be modified in-game options menu

--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+    public float mouseSensitivity = 130f;
+
     float mouse_x;
     float mouse_y;
     public Transform playerBody;
@@ -15,21 +17,14 @@ public class CameraControls : MonoBehaviour
     }
 
     void Update(){
-        mouse_x = Input.GetAxis("Mouse X") * 130f * Time.deltaTime;
-        mouse_y = Input.GetAxis("Mouse Y") * 130f * Time.deltaTime;
+        
+        mouse_x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouse_y = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
         rotation_x -= mouse_y;
         rotation_x = Mathf.Clamp(rotation_x, -90f, 90f);
         rotation_y += mouse_x;
-        playerBody.transform.localRotation = Quaternion.Euler(rotation_x, rotation_y, 0f);
-        //playerBody.Rotate(Vector3.up * mouse_x);
-    }
 
-    /*LateUpdate is called after all Update functions have been called. This is useful to order script execution. 
-    For example a follow camera should always be implemented in LateUpdate because it tracks objects that might have moved inside Update.*/
-    /*private void LateUpdate()
-    {
-        
-        //transform.localRotation = Quaternion.Euler(rotation_x, 0f, 0f);
-        //playerBody.Rotate(Vector3.up * mouse_x);
-    }*/
+        playerBody.transform.localRotation = Quaternion.Euler(rotation_x, rotation_y, 0f);
+    }
 }

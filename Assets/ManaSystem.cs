8707d711db959da050417130f1809f083dd3ce62
miko859 <155954510+miko859@users.noreply.github.com>
2024-10-08 13:26:05 +0200

    manaBar Regen

--- Assets/ManaSystem.cs
+++ Assets/ManaSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,13 +8,29 @@ public class ManaSystem : MonoBehaviour
     public int maxMana = 100;
     public int currentMana;
     public int fireballManaCost = 10;
+    public int manaRegen = 1;
+    public float regenInterval = 0.2f;
 
     void Start()
     {
         currentMana = maxMana;
         UpdateManaUI();
-    }
 
+        StartCoroutine(RegenerateMana());
+    }
+    IEnumerator RegenerateMana()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(regenInterval);
+
+            if (currentMana < maxMana)
+            {
+                currentMana = Mathf.Min(currentMana + manaRegen, maxMana); 
+                UpdateManaUI();
+            }
+        }
+    }
     public bool SpendMana(int amount)
     {
         if (currentMana >= amount)

5a899ec35403c36754fe8fe9bd8a8549909e77b1
miko859 <miko.doro@gmail.com>
2024-11-27 09:30:19 +0100

    flamethrower start fix

--- Assets/Scripts/Spells/Elements/FlameThrower.cs
+++ Assets/Scripts/Spells/Elements/FlameThrower.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class Flamethrower : Element
 {
-
+    public void Start()
+    {
+        particleSystem.Stop();
+    }
     public override void Initialize()
     {
         manaCost = 5;

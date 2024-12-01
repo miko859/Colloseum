ad8f169dc15c3a5fdc5d66e6c7fa66645f21b497
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2024-12-01 09:56:59 +0100

    dropbar

--- Assets/Scripts/SceneScrips/Scenes/MainMenu.cs
+++ Assets/Scripts/SceneScrips/Scenes/MainMenu.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject options;
+    public GameObject optionsGameplay;
+    public GameObject optionsVideo;
+    public GameObject optionsAudio;
+    public GameObject optionsCotrolls;
+    public GameObject optionsBack;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -10,11 +17,16 @@ public class MainMenu : MonoBehaviour
 
     public void Options()
     {
-
+        options.SetActive(true);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        options.SetActive(false);
     }
 }

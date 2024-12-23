0cc1596a78b4815ca4dc37c0c95c9f302baa45ed
miko859 <miko.doro@gmail.com>
2024-12-23 16:31:06 +0100

    new sound for light and heavy attack

--- Assets/Scripts/Items/Weapons/WeaponAnimations.cs
+++ Assets/Scripts/Items/Weapons/WeaponAnimations.cs
@@ -1,13 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WeaponAnimations : Weapon
 {
     private bool isBlocking = false;
-    
+
     private float damage;
     private float buffDebuffDmg = 0;
-    
+
+    // Audio-related fields
+    private AudioSource audioSource;
+    public AudioClip lightAttackSound;
+    public AudioClip heavyAttackSound;
+    public AudioClip blockSound;
+    public AudioClip bashSound;
+    public AudioClip hurtSound;
 
     public void ChangeBuffDebuffDmg(float value)
     {
@@ -16,7 +24,7 @@ public class WeaponAnimations : Weapon
 
     public float getDamage()
     {
-        return damage; 
+        return damage;
     }
 
     public bool getIsBashing()
@@ -29,32 +37,42 @@ public class WeaponAnimations : Weapon
         return isBlocking;
     }
 
+
+
     protected override void Awake()
     {
         base.Awake();
+        audioSource = GetComponent<AudioSource>(); // Ensure an AudioSource component is attached to the GameObject
     }
 
     /// <summary>
-    /// Basic Attacl/Light Attack
+    /// Basic Attack/Light Attack
     /// </summary>
     public override void Attack()
     {
-        
         if (!isBlocking)
         {
             if (!isAttacking)
             {
                 isAttacking = true;
                 damage = weaponData.lightAttackDamage + buffDebuffDmg;
-                var attackType = Random.Range(1, weaponData.lightAttackTypesCount+1);
-                StartCoroutine(PlayAnimation(weaponData.lightAttackAnimation + attackType, (attackType == 1) ? weaponData.lightAttackDuration1 : ((attackType == 2) ? weaponData.lightAttackDuration2 : weaponData.lightAttackDuration3)));
-                StartCoroutine(PlayBodyAnimation(weaponData.lightAttackAnimation + attackType, (attackType == 1) ? weaponData.lightAttackDuration1 : ((attackType == 2) ? weaponData.lightAttackDuration2 : weaponData.lightAttackDuration3)));
+                var attackType = Random.Range(1, weaponData.lightAttackTypesCount + 1);
+                StartCoroutine(PlayAnimation(weaponData.lightAttackAnimation + attackType,
+                    (attackType == 1) ? weaponData.lightAttackDuration1 :
+                    ((attackType == 2) ? weaponData.lightAttackDuration2 : weaponData.lightAttackDuration3)));
+                StartCoroutine(PlayBodyAnimation(weaponData.lightAttackAnimation + attackType,
+                    (attackType == 1) ? weaponData.lightAttackDuration1 :
+                    ((attackType == 2) ? weaponData.lightAttackDuration2 : weaponData.lightAttackDuration3)));
+
+                // Play the light attack sound with a delay
+                StartCoroutine(PlaySoundWithDelay(lightAttackSound, 0.3f));
             }
         }
     }
 
+
     /// <summary>
-    /// HARD Attack/Strong Attack
+    /// Hard Attack/Strong Attack
     /// </summary>
     /// <param name="attackPhase"></param>
     public override void HardAttack(bool attackPhase)
@@ -66,13 +84,15 @@ public class WeaponAnimations : Weapon
                 damage = weaponData.heavyAttackDamage + buffDebuffDmg;
                 StartCoroutine(PlayAnimation(weaponData.heavyAttackAnimation, weaponData.heavyAttackChargeDuration));
                 StartCoroutine(PlayBodyAnimation(weaponData.heavyAttackAnimation, weaponData.heavyAttackChargeDuration));
+
             }
             else
             {
-                StartCoroutine(PlayAnimation(weaponData.heavyAttackPerformAnimation, weaponData.heavyAttackPerformDuration));   
+                StartCoroutine(PlayAnimation(weaponData.heavyAttackPerformAnimation, weaponData.heavyAttackPerformDuration));
                 StartCoroutine(PlayBodyAnimation(weaponData.heavyAttackPerformAnimation, weaponData.heavyAttackPerformDuration));
-            }
+                PlaySound(heavyAttackSound); // Play heavy attack sound
 
+            }
         }
     }
 
@@ -84,10 +104,11 @@ public class WeaponAnimations : Weapon
         isBlocking = !isBlocking;
         animator.SetBool("Block", isBlocking);
         GetBodyAnimator().SetBool("Block", isBlocking);
+        PlaySound(blockSound); // Play block sound
     }
 
     /// <summary>
-    /// Bash, player will bash enemy, which will stun enemy for little time
+    /// Bash, player will bash enemy, which will stun enemy for a little time
     /// </summary>
     public override void Bash()
     {
@@ -96,6 +117,7 @@ public class WeaponAnimations : Weapon
             StartCoroutine(GetComponent<WeaponManager>().SwapCollBlockBash());
             StartCoroutine(PlayAnimation(weaponData.bashAnimation, weaponData.bashDuration));
             StartCoroutine(PlayBodyAnimation(weaponData.bashAnimation, weaponData.bashDuration));
+            PlaySound(bashSound); // Play bash sound
         }
     }
 
@@ -123,6 +145,7 @@ public class WeaponAnimations : Weapon
     {
         StartCoroutine(PlayAnimation(weaponData.hurtAnimation, weaponData.hurtDuration));
         StartCoroutine(PlayBodyAnimation(weaponData.hurtAnimation, weaponData.hurtDuration));
+        PlaySound(hurtSound); // Play hurt sound
     }
 
     public override void Equip()
@@ -134,4 +157,32 @@ public class WeaponAnimations : Weapon
     {
         gameObject.SetActive(false);
     }
+
+    /// <summary>
+    /// Play a given sound effect
+    /// </summary>
+    /// <param name="clip">Audio clip to play</param>
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    private IEnumerator PlaySoundWithDelay(AudioClip clip, float delay)
+    {
+        if (clip != null && audioSource != null)
+        {
+            yield return new WaitForSeconds(delay);
+            audioSource.PlayOneShot(clip); // Play the sound after the delay
+        }
+    }
+
+    private void PlaySoundWithDelay(AudioClip clip)
+    {
+        StartCoroutine(PlaySoundWithDelay(clip, 0.2f)); // Add a delay of 0.2 seconds
+    }
+
+
 }

979d5d37bf54a62251e8c80bcc0c4d9e12622a29
Róbert Lumcer <robert.lumcer@kosickaakademia.sk>
2025-01-14 11:25:39 +0100

    tests

---
+++ Assets/Test_EditMode/Traps_Tests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Traps_Tests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void Traps_TestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
